00e85e9dbcd14ea63acae0cbfc6092f0c2f328d6
brightonchege <brightonspak@gmail.com>
2022-08-14 00:16:50 +0300

    key framesfun

---
+++ index.js
@@ -0,0 +1,8 @@
+const computer = document.getElementsByClassName('coputerchoice')
+const user = document.getElementsByClassName('userchoice')
+const winner = document.getElementsByClassName('winner')
+const buttonss = document.querySelectorAll('button')
+let me
+let it
+buttonss.forEach(b
+
