4c0062b33f8a6fe03b17a06dae53dbadcd5f30c2
brighton <brightonspak@gmail.com>
2022-08-27 15:07:30 +0300

    personal portfolio

--- index.js
+++
@@ -1,8 +0,0 @@
-const computer = document.getElementsByClassName('coputerchoice')
-const user = document.getElementsByClassName('userchoice')
-const winner = document.getElementsByClassName('winner')
-const buttonss = document.querySelectorAll('button')
-let me
-let it
-buttonss.forEach(b
-
